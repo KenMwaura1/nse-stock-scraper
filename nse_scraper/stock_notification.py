848c09e61d9eea2f1f69223dd72cc6fee226efac
KenMwaura1 <kemwaura@gmail.com>
2023-03-26 15:15:03 +0300

    new updaTE

--- nse_scraper/stock_notification.py
+++ nse_scraper/stock_notification.py
@@ -1,38 +1,24 @@
-from typing import List, Any
 
-from models import StockData, db_connect
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import desc
+
 import africastalking as at
 import os
 from dotenv import load_dotenv
+import pymongo
 
 load_dotenv()
+
 at_username = os.getenv("at_username")
 at_api_key = os.getenv("at_api_key")
 mobile_number = os.getenv("mobile_number")
+mongo_uri = os.getenv("MONGODB_URI")
 # print(at_username, at_api_key)
 # Initialize the Africas sdk py passing the api key and username from the .env file
 at.initialize(at_username, at_api_key)
 sms = at.SMS
 account = at.Application
 
-engine = db_connect()
-Session = sessionmaker(bind=engine)
-session = Session()
+
 ticker_data = []
-
-
-# TODO: Add a function to notify client once the price changes
-def stock_query():
-    sq = session.query(StockData.id, StockData.stock_ticker, StockData.stock_name,
-                       StockData.stock_price).filter(StockData.stock_ticker == "SCOM")
-
-    for id, ticker, name, price in sq.order_by(desc(StockData.id)).limit(1):
-        # print(id, name, ticker, price)
-        new_data = {'stock_id': id, 'stock_name': name, 'stock_ticker': ticker, 'stock_price': price}
-        return new_data
-
 
 # Create a function to send a message containing the stock ticker and price
 def stock_notification(message: str, number: int):
@@ -44,9 +30,32 @@ def stock_notification(message: str, number: int):
         print(f" Houston we have a problem: {e}")
 
 
-message = f'{stock_query()["stock_name"]}, is now Kes {stock_query()["stock_price"]} per share'
-# check if Safaricom share price is more than Kes 39 and send a notification.
-if stock_query().get("stock_price") >= 38:
-    # Call the function passing the message  and mobile_number as a arguments
-    print(message)
-    stock_notification(message, mobile_number)
+# TODO: Add a function to notify client once the price changes
+# create a function to query mongodb for the stock price of Safaricom
+def stock_query():
+    client = pymongo.MongoClient(mongo_uri)
+    db = client["nse_data"]
+    collection = db["stock_data"]
+    # print(collection.find_one())
+    ticker_data = collection.find_one({"ticker": "BAT"})
+    print(ticker_data)
+    stock_name = ticker_data["name"]
+    stock_price = ticker_data["price"]
+    sms_data = { "stock_name": stock_name, "stock_price": stock_price }
+    print(sms_data)
+
+    message = f"Hello the current stock price of {stock_name} is {stock_price}"
+    # check if Safaricom share price is more than Kes 39 and send a notification.
+    if int(float(stock_price)) >= 38:
+        # Call the function passing the message  and mobile_number as a arguments
+        print(message)
+        stock_notification(message, mobile_number)
+    else:
+        print("No notification sent")
+    
+    client.close()
+
+    return sms_data
+
+stock_query()
+
