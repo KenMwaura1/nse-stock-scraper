24af3b23f374732f89c2e8a8ca065fa125be56f2
KenMwaura1 <kemwaura@gmail.com>
2023-03-26 07:13:58 +0300

    +updated items

--- nse_scraper/items.py
+++ nse_scraper/items.py
@@ -1,14 +1,10 @@
-# Define here the models for your scraped items
-
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
-
 from scrapy.item import Item, Field
 
 
 class NseScraperItem(Item):
     # define the fields for your item here like:
-    ticker = Field()
-    name = Field()
-    price = Field()
+    ticker_symbol = Field()
+    stock_name = Field()
+    stock_price = Field()
+    stock_change = Field()
+    
